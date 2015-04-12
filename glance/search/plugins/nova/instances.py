d2a8488ae8878aa1af6535f3a9bc8d2bba6bcde0
Steve McLellan <steven.j.mclellan@gmail.com>
2015-04-11 19:50:20 -0500

    Adds nova plugin
    Adds a nova plugin, including support for initial loading
    based off the nova API and via notifications from nova. Since
    both these methods provide quite different information, there's
    some work to do.

---
+++ glance/search/plugins/nova/instances.py
@@ -0,0 +1,145 @@
+# Copyright (c) 2014 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from oslo_config import cfg
+from novaclient.v2 import client as nc_client
+
+from glance.search.plugins import base
+from . import instances_notification_handler
+
+CONF = cfg.CONF
+
+
+class InstanceIndex(base.IndexBase):
+    def __init__(self):
+        super(InstanceIndex, self).__init__()
+
+        self._instance_base_properties = [
+            'id', 'name', 'status', 'power_state', 'owner'
+        ]
+
+    def get_index_name(self):
+        return 'nova'
+
+    def get_document_type(self):
+        return 'instance'
+
+    def get_mapping(self):
+        return {
+            'dynamic': True,
+            'properties': {
+                'id': {'type': 'string', 'index': 'not_analyzed'},
+                'instance_id': {'type': 'string', 'index': 'not_analyzed'},
+                'name': {'type': 'string'},
+                # TODO - make flavor flat?
+                'flavor': {
+                    'type': 'nested',
+                    'properties': {
+                        'id': {'type': 'string', 'index': 'not_analyzed'},
+                        'name': {'type': 'string', 'index': 'not_analyzed'},
+                        }
+                },
+                'owner': {'type': 'string', 'index': 'not_analyzed'},
+                'created_at': {'type': 'date'},
+                'updated_at': {'type': 'date'},
+                'networks': {
+                    'type': 'nested',
+                    'properties': {
+                        'name': {'type': 'string'},
+                        'ipv4': {'type': 'ip'}
+                    }
+                },
+                'image': {
+                    'type': 'nested',
+                    'properties': {
+                        'id': {'type': 'string', 'index': 'not_analyzed'},
+                        'container_format': {'type': 'string', 'index': 'not_analyzed'},
+                        'min_ram': {'type': 'integer'},
+                        'disk_format': {'type': 'string', 'index': 'not_analyzed'},
+                        'min_disk': {'type': 'integer'},
+                        'kernel_id': {'type': 'string', 'index': 'not_analyzed'},
+                        'image_id': {'type': 'string', 'index': 'not_analyzed'} # base_image_ref
+                    }
+                },
+                'state_description': {'type': 'string'},
+                'availability_zone': {'type': 'string', 'index': 'not_analyzed'},
+                'status': {'type': 'string', 'index': 'not_analyzed'},
+                'disk_format': {'type': 'string', 'index': 'not_analyzed'},
+                'memory_mb': {'type': 'integer'},
+                'vcpus': {'type': 'integer'},
+                'disk_gb': {'type': 'integer'},
+            },
+        }
+
+    def get_rbac_filter(self, request_context):
+        return [
+            {
+                "and": [
+                    {
+                        'term': {
+                            'owner': request_context.owner
+                        }
+                    },
+                    {
+                        'type': {
+                            'value': self.get_document_type()
+                        }
+                    }
+                ]
+            }
+        ]
+
+    def get_objects(self):
+        with nc_client.Client(CONF.os_username,
+                              CONF.os_password,
+                              CONF.os_tenant_name,
+                              CONF.os_auth_url) as nc:
+            # TODO: paging etc
+            return nc.servers.list()
+
+    def serialize(self, server):
+        return dict(
+            id=server.id,
+            instance_id=server.id,
+            name=server.name,
+            status=server.status.lower(),
+            owner=server.tenant_id,
+            updated=server.updated,
+            created=server.created,
+            networks=server.networks,
+            availability_zone=getattr(server, 'OS-EXT-AZ:availability_zone', None),
+            image=dict(
+                # TODO: get the rest
+                id=server.image['id']
+            ),
+            flavor=dict(
+                id=server.flavor['id']
+            )
+        )
+
+    def get_notification_handler(self):
+        return instances_notification_handler.InstanceHandler(
+            self.engine,
+            self.get_index_name(),
+            self.get_document_type()
+        )
+
+    @staticmethod
+    def get_notification_topic_exchange():
+        return 'notifications', 'nova'
+
+    def get_notification_supported_events(self):
+        # TODO: DRY
+        return ['compute.instance.update']
---
+++ glance/search/plugins/nova/instances_notification_handler.py
@@ -0,0 +1,93 @@
+# Copyright (c) 2014 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import datetime
+from oslo_log import log as logging
+import oslo_messaging
+
+from glance.search.plugins import base
+from glance.common import utils
+
+LOG = logging.getLogger(__name__)
+
+
+class InstanceHandler(base.NotificationBase):
+    """Handles nova server notifications. These can come as a result of
+    a user action (like a name change, state change etc) or as a result of
+    periodic auditing notifications nova sends
+    """
+    def __init__(self, *args, **kwargs):
+        super(InstanceHandler, self).__init__(*args, **kwargs)
+        self.image_delete_keys = ['deleted_at', 'deleted',
+                                  'is_public', 'properties']
+
+    def process(self, ctxt, publisher_id, event_type, payload, metadata):
+        try:
+            actions = {
+                # compute.instance.update seems to be the event set as a
+                # result of a state change etc
+                "compute.instance.update": self.create,
+            }
+            actions[event_type](payload)
+            return oslo_messaging.NotificationResult.HANDLED
+        except Exception as e:
+            LOG.error(utils.exception_to_str(e))
+
+    def create(self, payload):
+        id = payload['instance_id']
+        payload = self.format_server(payload)
+        doc = {"doc": payload}
+        self.engine.index(
+            index=self.index_name,
+            doc_type=self.document_type,
+            body=doc,
+            id=id
+        )
+
+    def format_server(self, payload):
+        # TODO: Maybe the index should be more similar to the notification
+        # structure? Notifications have a LOT more information than do
+        # what we can get from a single nova call, though missing some stuff,
+        # notably networking info
+        # https://wiki.openstack.org/wiki/SystemUsageData#compute.instance.update:
+        print payload
+        return dict(
+            id=payload['instance_id'],
+            instance_id=payload['instance_id'],
+            name=payload['display_name'],
+            status=payload['state'],
+            owner=payload['tenant_id'],
+            updated=datetime.datetime.utcnow(), # TODO: Not this.
+            created=payload['created_at'],
+            # networks=server.networks,  # TODO: Figure this out
+            availability_zone=payload.get('availability_zone', None),
+            image=dict(
+                id=payload['image_meta']['base_image_ref'],
+                kernel_id=payload['image_meta']['kernel_id'],
+                #name=bizarrely not here
+                container_format=payload['image_meta']['container_format'],
+                disk_format=payload['image_meta']['disk_format'],
+                min_disk=payload['image_meta']['min_disk'],
+                min_ram=payload['image_meta']['min_ram'],
+            ),
+            flavor=dict(
+                id=payload['instance_flavor_id'],
+                name=payload['instance_type'],
+            ),
+            state_description=payload['state_description'],
+            vcpus=payload['vcpus'],
+            disk_gb=payload['disk_gb'],
+            memory_mb=payload['memory_mb'],
+        )
